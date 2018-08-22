3302117c1de1361a34168254d69b78f4b47daab9
spacemeowx2 <spacemeowx2@gmail.com>
2018-08-23 01:09:09 +0800

    Hello world

---
+++ README.md
@@ -0,0 +1,10 @@
+# Build
+
+`sudo apt install libpcap0.8-dev`
+
+```sh
+mkdir build
+cd build
+cmake ..
+make
+```
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <pcap.h>
+
+int main()
+{
+    puts("Hello world");
+    return 0;
+}
