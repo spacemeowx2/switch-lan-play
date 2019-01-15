0c1cc2abc3f0d5ae212fee105667e28cb7bff95a
spacemeowx2 <spacemeowx2@gmail.com>
2019-01-15 12:49:46 +0800

    fix `make install` fails (#42). only install `lan-play`

--- external/external.cmake
+++ external/external.cmake
@@ -1,11 +1,12 @@
+
 message(STATUS "Installing libuv via submodule")
 execute_process(COMMAND git submodule update --init -- external/libuv
                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-add_subdirectory(external/libuv)
+add_subdirectory(external/libuv EXCLUDE_FROM_ALL)
 target_include_directories(uv_a INTERFACE external/libuv/include)
 
 message(STATUS "Installing uvw via submodule")
 execute_process(COMMAND git submodule update --init -- external/uvw
                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-add_subdirectory(external/uvw)
+add_subdirectory(external/uvw EXCLUDE_FROM_ALL)
 include_directories(external/uvw/src)
