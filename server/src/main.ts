597b4a11d9319536894d9fa84eedf45c0a53422d
Dong,WANG <dong.wang@anz.com>
2020-04-13 12:20:07 +0800

    [TECH][Dong] use cli parameter instead of question

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,77 +1,45 @@
 import { SLPServer } from './udpserver'
 import { ServerMonitor } from './monitor'
-import { AuthProvider, JsonAuthProvider, HttpAuthProvider } from './auth'
+import { AuthProvider, HttpAuthProvider, JsonAuthProvider } from './auth'
 import { CustomAuthProvider } from "./auth/CustomAuthProvider";
 
-function startServer(argv: string[], port: string | undefined, username: string | undefined, password: string | undefined) {
-  if (!port) {
-    port = argv[0] || '11451';
+function parseArgs2Obj(args: string[]) {
+  let argsObj: any = {};
+  for (let i = 0; i < args.length; i += 2) {
+    let key: string = args[i];
+    let value: string = args[i + 1];
+    if (key.startsWith('--')) {
+      argsObj[key.slice(2)] = value;
+    }
   }
-  let jsonPath = argv[1];
+  return argsObj;
+}
+
+function main(argv: string[]) {
+  let argsObj = parseArgs2Obj(argv);
+
   let provider: AuthProvider | undefined
   const {USE_HTTP_PROVIDER} = process.env
   if (USE_HTTP_PROVIDER) {
     provider = new HttpAuthProvider(USE_HTTP_PROVIDER)
     console.log(`using HttpAuthProvider url: ${USE_HTTP_PROVIDER}`)
-  } else if (jsonPath) {
-    provider = new JsonAuthProvider(jsonPath)
-    console.log(`using JsonAuthProvider file: ${jsonPath}`)
-  } else if (username && password) {
+  } else if (argsObj.httpAuth) {
+    provider = new HttpAuthProvider(argsObj.httpAuth)
+    console.log(`using HttpAuthProvider url: ${argsObj.httpAuth}`)
+  } else if (argsObj.jsonAuth) {
+    provider = new JsonAuthProvider(argsObj.jsonAuth)
+    console.log(`using JsonAuthProvider file: ${argsObj.jsonAuth}`)
+  } else if (argsObj.simpleAuth) {
+    let username_password = argsObj.simpleAuth.split(':');
+    let username = username_password[0];
+    let password = username_password[1];
     provider = new CustomAuthProvider(username, password);
-    console.log(`using custom auth with username=${username} password=******`);
+    console.log(`using simple auth with username=${username} password=${password}`);
   }
-  const portNum = parseInt(port)
+  const portNum = parseInt(argsObj.port || '11451')
   let s = new SLPServer(portNum, provider)
   let monitor = new ServerMonitor(s)
   monitor.start(portNum)
-}
-
-function main(argv: string[]) {
-  const readLine = require('readline');
-  const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  let port: string | undefined;
-  const getPort = () => {
-    return new Promise((resolve, reject) => {
-      rl.question('Please input the server port(default is 11451 or your first parameter) :', (answer: string) => {
-        port = answer;
-        resolve();
-      })
-    })
-  }
-
-  let username: string | undefined;
-  const getUsername = () => {
-    return new Promise((resolve, reject) => {
-      rl.question('Please input the auth username(default is empty) :', (answer: string) => {
-        username = answer;
-        resolve();
-      })
-    })
-  }
-
-  let password: string | undefined;
-  const getPassword = () => {
-    return new Promise((resolve, reject) => {
-      rl.question('Please input the auth password(default is empty) :', (answer: string) => {
-        password = answer;
-        resolve();
-      })
-    })
-  }
-
-  const go = async () => {
-    await getPort();
-    await getUsername();
-    await getPassword();
-    startServer(argv, port, username, password);
-    rl.close();
-  }
-
-  go();
 }
 
 main(process.argv.slice(2))
