1c4d887b7aae1fa3c4af68a89590f7608d99773d
spacemeowx2 <spacemeowx2@gmail.com>
2020-05-06 20:16:10 +0800

    fix: simple auth should accept plain text pw

--- server/src/auth/CustomAuthProvider.ts
+++ server/src/auth/CustomAuthProvider.ts
@@ -1,14 +1,16 @@
 import { BasicAuthProvider, AuthError, AuthErrorType, SHA1 } from './types'
 
 export class CustomAuthProvider extends BasicAuthProvider {
+  private sha1: Buffer
   constructor(private username: string, private password: string) {
     super()
+    this.sha1 = SHA1(this.password)
   }
 
   async getUserPasswordSHA1(username: string) {
     if (username !== this.username) {
       throw new AuthError(AuthErrorType.NoSuchUser, 'No such user')
     }
-    return Buffer.from(this.password, 'hex')
+    return this.sha1
   }
 }
